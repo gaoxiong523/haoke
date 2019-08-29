285ec5f857f5b403784f60af7d0e7db9345f9509
gaoxiong <gaoxiong5233@163.com>
2019-08-29 23:21:01 +0800

    redis 本机虚拟机集群成功

--- gaoxing-cloud-zuul-ratelimit/src/test/java/com/gaoxiong/GaoxiongRateLimitTest.java
+++ gaoxing-cloud-zuul-ratelimit/src/test/java/com/gaoxiong/GaoxiongRateLimitTest.java
@@ -4,8 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.connection.RedisServer;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -18,6 +22,15 @@ public class GaoxiongRateLimitTest {
 
     @Test
     public void test1(){
+        Collection<RedisServer> masters = redisTemplate.getConnectionFactory().getSentinelConnection().masters();
+        for (RedisServer master : masters) {
+            System.out.println("master = " + master);
+        }
+        List clientList = redisTemplate.getClientList();
+        for (Object o : clientList) {
+            System.out.println("o = " + o);
+        }
+
         redisTemplate.opsForValue().set("test","gaoxiong" );
     }
 
